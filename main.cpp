e2bcb4d9a062a7f348cf56daef66e781e5ec9b30
Pimenta <matheuscscp@gmail.com>
2014-07-20 18:35:27 -0300

    Refactoring code

--- main.cpp
+++ main.cpp
@@ -19,113 +19,106 @@ using namespace std;
 
 typedef int64_t  word_t;
 typedef uint64_t uword_t;
-typedef int32_t  address_t;
+typedef uint32_t address_t;
 
 enum field_t {
   A, B, J
 };
 
-typedef map<string, list<pair<address_t, field_t>>>::iterator map_iterator;
-typedef list<pair<address_t, field_t>>::iterator list_iterator;
-
 #ifndef MEM_WORDS
 #define MEM_WORDS 0x2000
 #endif
 
-const address_t ADDRESS_MASK  = MEM_WORDS - 1;
-const uword_t   ADDRESS_WIDTH = log2(MEM_WORDS);
-const uword_t   A_MASK        = ~(((uword_t)(MEM_WORDS - 1)) << 2*ADDRESS_WIDTH);
-const uword_t   B_MASK        = ~(((uword_t)(MEM_WORDS - 1)) << 1*ADDRESS_WIDTH);
-const uword_t   J_MASK        = ~(((uword_t)(MEM_WORDS - 1)) << 0*ADDRESS_WIDTH);
+const uword_t ADDRESS_WIDTH = uword_t(log2(double(MEM_WORDS)));
 
 int main(int argc, char* argv[]) {
   if (argc != 3) {
-    fprintf(stderr, "Usage mode: subleq-asm <subleq_assembly_file> <binary_output>\n");
+    fprintf(stderr, "Usage mode: subleq-asm <assembly_file> <object_file>\n");
     return 0;
   }
   
   fstream f;
   string buf;
-  uword_t word;
   set<string> exported;
   map<string, address_t> symbols;
   map<string, list<pair<address_t, field_t>>> references;
-  address_t ip = 0;
-  uword_t mem[MEM_WORDS];
+  address_t code_size = 0;
+  uword_t* mem = new uword_t[MEM_WORDS];
   
   f.open(argv[1]);
   
-  while (f.get() != '\n'); // skip "{.export\n}"
+  f >> buf; // reading ".export"
   
-  do {
-    f >> buf;
-    if (buf != ".text")
-      exported.insert(buf);
-  } while (buf != ".text");
+  for (f >> buf; buf != ".data"; f >> buf) { // reading export section
+    exported.insert(buf);
+  }
   
-  while (f.get() != '\n'); // skip ".text{\n}"
+  for (f >> buf; buf != ".text"; f >> buf) { // reading data section
+    symbols[buf.substr(0, buf.size() - 1)] = code_size;
+    f >> mem[code_size++];
+  }
   
-  do {
-    f >> buf;
-    if (buf[buf.size() - 1] == ':') {
-      symbols[buf.substr(0, buf.size() - 1)] = ip;
-      if (buf == "start:")
-        exported.insert(buf.substr(0, buf.size() - 1));
+  for (f >> buf; !f.eof(); f >> buf) { // reading text section
+    if (buf[buf.size() - 1] == ':') { // label found
+      symbols[buf.substr(0, buf.size() - 1)] = code_size;
+      
+      if (buf == "start:") // exporting start label
+        exported.insert(string("start"));
+      
+      continue;
     }
-    else if (buf != ".data") {
-      word = 0;
-      if (symbols.find(buf) == symbols.end())
-        references[buf].push_back(pair<address_t, field_t>(ip, A));
-      else
-        word |= ((uword_t)((symbols[buf] - ip) & ADDRESS_MASK)) << 2*ADDRESS_WIDTH;
-      f >> buf;
-      if (symbols.find(buf) == symbols.end())
-        references[buf].push_back(pair<address_t, field_t>(ip, B));
-      else
-        word |= ((uword_t)((symbols[buf] - ip) & ADDRESS_MASK)) << 1*ADDRESS_WIDTH;
-      f >> buf;
-      if (symbols.find(buf) == symbols.end())
-        references[buf].push_back(pair<address_t, field_t>(ip, J));
-      else
-        word |= ((uword_t)((symbols[buf] - ip) & ADDRESS_MASK)) << 0*ADDRESS_WIDTH;
-      mem[ip++] = word;
-    }
-  } while (buf != ".data");
-  
-  while (f.get() != '\n'); // skip ".data{\n}"
-  
-  do {
+    
+    uword_t instr = 0;
+    
+    // field A
+    auto sym = symbols.find(buf);
+    if (sym == symbols.end())
+      references[buf].emplace_back(code_size, A);
+    else
+      instr |= (uword_t(sym->second) << 2*ADDRESS_WIDTH);
+    
+    // field B
     f >> buf;
-    if (!f.fail()) {
-      symbols[buf] = ip;
-      f >> mem[ip++];
-    }
-  } while (!f.eof());
+    sym = symbols.find(buf);
+    if (sym == symbols.end())
+      references[buf].emplace_back(code_size, B);
+    else
+      instr |= (uword_t(sym->second) << 1*ADDRESS_WIDTH);
+    
+    // field J
+    f >> buf;
+    sym = symbols.find(buf);
+    if (sym == symbols.end())
+      references[buf].emplace_back(code_size, J);
+    else
+      instr |= (uword_t(sym->second) << 0*ADDRESS_WIDTH);
+    
+    mem[code_size++] = instr;
+  }
   
   f.close();
   
   // solve references
-  for (map_iterator map_it = references.begin(); map_it != references.end();) {
+  for (auto map_it = references.begin(); map_it != references.end();) {
     // external symbols
-    if (symbols.find(map_it->first) == symbols.end()) {
+    auto sym = symbols.find(map_it->first);
+    if (sym == symbols.end()) {
       ++map_it;
       continue;
     }
     
     // solve
-    for (list_iterator list_it = map_it->second.begin(); list_it != map_it->second.end(); ++list_it) {
-      switch (list_it->second) {
+    for (auto it = map_it->second.begin(); it != map_it->second.end(); ++it) {
+      uword_t& instr = mem[it->first];
+      switch (it->second) {
         case A:
-          mem[list_it->first] &= A_MASK;
-          mem[list_it->first] |= ((uword_t)((symbols[map_it->first] - list_it->first) & ADDRESS_MASK)) << 2*ADDRESS_WIDTH;
+          instr |= (uword_t(sym->second) << 2*ADDRESS_WIDTH);
           break;
         case B:
-          mem[list_it->first] &= B_MASK;
-          mem[list_it->first] |= ((uword_t)((symbols[map_it->first] - list_it->first) & ADDRESS_MASK)) << 1*ADDRESS_WIDTH;
+          instr |= (uword_t(sym->second) << 1*ADDRESS_WIDTH);
           break;
         case J:
-          mem[list_it->first] &= J_MASK;
-          mem[list_it->first] |= ((uword_t)((symbols[map_it->first] - list_it->first) & ADDRESS_MASK)) << 0*ADDRESS_WIDTH;
+          instr |= (uword_t(sym->second) << 0*ADDRESS_WIDTH);
           break;
       }
     }
@@ -178,13 +171,15 @@ int main(int argc, char* argv[]) {
     }
     
     // write assembled code size
-    f.write((const char*)&ip, sizeof(address_t));
+    f.write((const char*)&code_size, sizeof(address_t));
     
     // write assembled code
-    f.write((const char*)mem, sizeof(uword_t)*ip);
+    f.write((const char*)mem, sizeof(uword_t)*code_size);
   }
   
   f.close();
+  
+  delete[] mem;
   
   return 0;
 }
