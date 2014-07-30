e991541fe245057720097d81781a9df3f32ab9eb
Alexandre Dantas <eu@alexdantas.net>
2014-07-30 18:23:32 -0300

    Added `safeGetline`

--- main.cpp
+++ main.cpp
@@ -72,6 +72,61 @@ static uword_t* mem = new uword_t[MEM_WORDS];
 
 static int currentLine = 1, lastTokenLine = 1, currentTokenLine = 1;
 
+/// Reads a string from `is` to the end-of-line, saving it on `t`.
+///
+/// @note It doesn't save the end-of-line character on `t`.
+///
+/// This is a "safe" implementation of `std::getline`.
+/// It is "safe" in the sense that it does work with all possible
+/// line endings ("\r", "\n" and "\r\n").
+///
+/// Taken straight from StackOverflow. Source:
+/// http://stackoverflow.com/a/6089413
+///
+std::istream& safeGetline(std::istream& is, std::string& t)
+{
+  t.clear();
+
+  // The characters in the stream are read one-by-one using a
+  // std::streambuf. That is faster than reading them one-by-one
+  // using the std::istream.
+  //
+  // Code that uses streambuf this way must be guarded by a sentry
+  // object.
+  // The sentry object performs various tasks, such as thread
+  // synchronization and updating the stream state.
+
+  std::istream::sentry se(is, true);
+
+  std::streambuf* sb = is.rdbuf();
+
+  while (true) {
+    int c = sb->sbumpc();
+
+    switch (c) {
+
+    case '\n':
+      return is;
+
+    case '\r':
+      if (sb->sgetc() == '\n')
+        sb->sbumpc();
+
+      return is;
+
+    case EOF:
+      // Also handle the case when the last line has no line ending
+      if (t.empty())
+        is.setstate(std::ios::eofbit);
+
+      return is;
+
+    default:
+      t += (char)c;
+    }
+  }
+}
+
 /// Reads a token (separated by whitespaces) from the file,
 /// char by char, putting on `buf`
 inline static void readToken() {
@@ -90,7 +145,7 @@ inline static void readToken() {
     char c = f.get();
 
     if (!f.good())
-	    break;
+      break;
 
     // comment found
     if (c == '/') {
